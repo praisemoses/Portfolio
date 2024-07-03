0d1e3f0a8c9082d186e3f1593f687b7e94d52e3a
praisemoses <praisemoses659@gmail.com>
2024-07-03 01:14:06 -0700

    final

--- port.js
+++ port.js
@@ -1,2 +1,21 @@
-let contactForm = document.querySelector("info.html");
-contactForm.setAttribute("type","text");
+let btn = document.getElementById("btn");
+btn.addEventListener("click", function() {
+    e.preventDefault() 
+    let name = document.getElementById("name").value;
+    let phone = document.getElementById("phone").value;
+    let email = document.getElementById("email").value;
+    let message = document.getElementById("message").value;
+    let body =  "name:" + name + "<br/> phone:" + phone + "<br/> email:" + email + "<br/> message:" + message;
+
+    Email.send({
+        Host : "smtp.gmail.com",
+        Username : "praisemoses659@gmail.com",
+        Password : "clfqwxbezarubqum",
+        To : "praisemoses659@gmail.com",
+        From : email,
+        Subject : phone,
+        Body : body
+    }).then(
+      message => alert(message)
+    );
+});
